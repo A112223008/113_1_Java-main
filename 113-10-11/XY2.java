3462c627e8ac493c269963cf7fc38c8f0085896f
A112223008 <a112223008@mail.shu.edu.tw>
2024-12-06 09:48:30 +0800

    113.10.11

---
+++ 113-10-11/XY2.java
@@ -0,0 +1,16 @@
+public class XY2 {
+    public static void main(String[] args) {
+        int[][] cords = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        for(int[] row : cords) {             //巡行二維陣列 ,x為列數
+            for(int c : row) {      //巡行二維陣列 ,y為列數,cords[x]為第x列的陣列
+                System.out.printf("%2d",c);
+            }
+            System.out.println();
+        }
+    }
+    
+}
